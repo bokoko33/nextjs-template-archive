6cf219698951df0ba4ac2a4b2c351f420d815858
Bokoko33 <kamabokoman1042@gmail.com>
2022-08-22 00:17:47 +0900

    setup

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const path = require('path');
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  optimizeFonts: false,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  webpack: (config) => {
+    // import vert/frag
+    config.module.rules.push({
+      test: /\.(vert|frag)$/i,
+      type: 'asset/source',
+    });
+
+    // Important: return the modified config
+    return config;
+  },
+};
