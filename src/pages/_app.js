d6aee80c501727e091252da10778363fee7983ef
bokoko33 <work.bokoko33@gmail.com>
2023-03-27 11:56:59 +0900

    appのhook呼んでなかったので追記

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,8 +2,11 @@ import 'destyle.css';
 import '~/styles/app.scss';
 import { OverlayContextProvider } from '~/context/OverlayContext';
 import { LayoutDefault } from '~/components/LayoutDefault';
+import { useMyApp } from '~/hooks/MyApp';
 
 function MyApp({ Component, pageProps }) {
+  useMyApp();
+
   return (
     <OverlayContextProvider>
       <LayoutDefault>
